dbf049fb67f93054ecf8ab77533fbfca73056da4
yusufadell <yusufadell.dev@gmail.com>
2023-02-15 00:45:14 +0200

    feat: cookiecutter new project dejavue

---
+++ dejavue/dejavue/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from dejavue.users.models import User
+from dejavue.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
